59449651254aa0fd015169d344908197ff5f349a
Rachel Adkins <rachelalaina1504@gmail.com>
2023-10-01 13:40:25 -0400

    Updated method in CupboardController

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/CupboardController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/CupboardController.java
@@ -7,7 +7,6 @@ import java.util.logging.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;

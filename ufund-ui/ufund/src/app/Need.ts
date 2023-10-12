a337439d49535a1bc79d4afe0e51ebac7fb93fcc
carlaLopez <carla.lopezsanchez20@gmail.com>
2023-10-11 21:36:50 -0400

    implemented service class

---
+++ ufund-ui/ufund/src/app/Need.ts
@@ -0,0 +1,7 @@
+export interface Need{
+    id:number;
+    name:string;
+    quantity:number;
+    description:string;
+    cost:number
+}
--- ufund-ui/ufund/src/app/needs.service.ts
+++ ufund-ui/ufund/src/app/needs.service.ts
@@ -1,9 +1,80 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, of, tap } from 'rxjs';
+import { Need } from './Need';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NeedsService {
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  private cupBoardURL ='http://localhost:8080/cupboard'
+  constructor(
+    private http: HttpClient,
+    ) 
+    { }
+
+
+    getNeedyId(id:number):Observable<Need>{
+      const url = `${this.cupBoardURL}/${id}`
+      return this.http.get<Need>(url,this.httpOptions)
+      .pipe(
+        catchError(this.handleError<Need>('getNeedbyId')))
+    }
+
+    createNeed(need:Need):Observable<Need>{
+      return this.http.post<Need>(this.cupBoardURL,need,this.httpOptions)
+      .pipe(
+        catchError(this.handleError<Need>('addNeed'))
+      );
+    }
+
+    deleteNeedByName(name:string):Observable<Need>{
+      const url = `${this.cupBoardURL}/${name}`
+      return this.http.delete<Need>(url,this.httpOptions).pipe(
+        catchError(this.handleError<Need>('deleteNeedbyName')))
+    }
+
+    deleteNeedbyId(id:number):Observable<Need>{
+      const url = `${this.cupBoardURL}/${id}`
+      return this.http.delete<Need>(url,this.httpOptions).pipe(
+        catchError(this.handleError<Need>('deleteNeedbyId')))
+    }
+
+    getEntireNeedsCupboard():Observable<Need[]>{
+      return this.http.get<Need[]>(this.cupBoardURL,this.httpOptions)
+      .pipe(
+        catchError(this.handleError<Need[]>('getCupboard', []))
+      );
+    }
+
+    updateNeed(need:Need):Observable<Need>{
+      return this.http.put<Need>(this.cupBoardURL,need,this.httpOptions)
+      .pipe(
+        catchError(this.handleError<Need>('updateNeed')))
+    }
+
+
+    searchCupboardByName(name:string):Observable<Need[]>{
+      if(!name.trim()){
+        return of([]);
+      }
+      return this.http.get<Need[]>(`${this.cupBoardURL}/?name=${name}`)
+      .pipe(
+        catchError(this.handleError<Need[]>('searchNeeds', []))
+      );
+
+    }
+
+private handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    console.error(error);
+    return of(result as T);
+  };
+}
 }

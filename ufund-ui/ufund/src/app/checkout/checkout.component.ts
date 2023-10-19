c851f9c55d55dd0d7e77795a698b9bc6976c742c
cheyzhangg <cheyzhangg@gmail.com>
2023-10-19 17:47:18 -0400

    removed needsService

--- ufund-ui/ufund/src/app/checkout/checkout.component.ts
+++ ufund-ui/ufund/src/app/checkout/checkout.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
 import { Need } from '../Need';
-import { NeedsService } from '../needs.service'
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
+
 export class CheckoutComponent {
   basket: Need[] = [];
-
-  constructor(
-    private needsService: NeedsService
-  ){}
 
   calculateTotal(): void{
     //add up total of checkout basket

d625c5d03c6745856c5a291d18ed20c372969ca6
Garrett Geyer <ggg5483@g.rit.edu>
2023-10-19 16:02:49 -0400

    updated login component to not have errors

--- ufund-ui/ufund/src/app/login/login.component.ts
+++ ufund-ui/ufund/src/app/login/login.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 
+//import { User } from '../User';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private needsService: NeedsService) { }
+  //constructor(private needsService: NeedsService) { }
+  //model: user = new User('');
+  loggedIn = false;
   login(): void{
-      this.needsService.login()
+      this.loggedIn = true;
   }
 }

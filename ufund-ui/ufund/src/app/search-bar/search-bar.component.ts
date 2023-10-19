02ceed8e992fef547f0313ad5633b397b1fa2eca
carlaLopez <carla.lopezsanchez20@gmail.com>
2023-10-18 22:46:08 -0400

    search bar component and dashboard component

--- ufund-ui/ufund/src/app/search-bar/search-bar.component.ts
+++ ufund-ui/ufund/src/app/search-bar/search-bar.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { NeedsService } from '../needs.service';
+import { Observable, Subject } from 'rxjs';
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
+
+import { Need } from '../Need';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit{
+  
+  needs$!:Observable<Need[]>;
+  private searchNeeds = new Subject<string>();
+
+  constructor(private needsService:NeedsService){}
+
+  search(value:string){
+    this.searchNeeds.next(value);
+  }
+  
+
+  ngOnInit(): void {
+    this.needs$ = this.searchNeeds.pipe(
+      debounceTime(300),
+
+      distinctUntilChanged(),
+
+      switchMap((term: string) => this.needsService.searchCupboardByName(term)),
+    );
+  }
 
 }
+

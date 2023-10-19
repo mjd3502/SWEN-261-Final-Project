35026d690dedcf7b01682a276e78594da0befbbd
carlaLopez <carla.lopezsanchez20@gmail.com>
2023-10-18 23:49:08 -0400

    added functionality for user to log out of dashboard

--- ufund-ui/ufund/src/app/helper-dashboard/helper-dashboard.component.ts
+++ ufund-ui/ufund/src/app/helper-dashboard/helper-dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { faBasketShopping } from '@fortawesome/free-solid-svg-icons';
 import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-helper-dashboard',
@@ -21,7 +22,25 @@ export class HelperDashboardComponent {
     console.log("checkbox clicked")
   }
 
+  redirectToPage(){
+    this.router.navigate(["login"])
+  }
+
   logOut(){
+    Swal.fire({
+      title:"Ready to leave?",
+      text:"Click below if you are ready to end your current session.",
+      showCancelButton:true,
+      showConfirmButton:true,
+      confirmButtonText:"Log out",
+    }).then((result =>{
+
+      if(result.isConfirmed){
+        this.redirectToPage();
+        console.log("log out")
+        
+      }
+    }))
     console.log("button working")
   }
 }

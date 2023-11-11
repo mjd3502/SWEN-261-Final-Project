0b6a75b7b0d596a32e8b5af740cd005cb37685ba
Garrett Geyer <ggg5483@g.rit.edu>
2023-11-11 15:27:08 -0500

    added service for favorite pets

--- ufund-ui/ufund/src/app/favorite-pets.service.ts
+++ ufund-ui/ufund/src/app/favorite-pets.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { User } from './User';
+import { Observable, catchError, of } from 'rxjs';
+import { Pet } from './Pet';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FavoritePets } from './FavoritePets';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoritePetsService {
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  private favoritePetsURL = 'http://localhost:8080/favoritePets';
+  
+  constructor(private http:HttpClient) { 
+
+  }
+
+  createFavoritePets(favoritePets:FavoritePets):Observable<FavoritePets> {
+    return this.http.post<FavoritePets>(this.favoritePetsURL,favoritePets,this.httpOptions)
+    .pipe(
+      catchError(this.handleError<FavoritePets>('createFavoritePets'))
+    );
+  }
+
+
+  getFavoritePets(name:string):Observable<Pet[]>{
+    const url = `${this.favoritePetsURL}/${name}`
+    return this.http.get<Pet[]>(url,this.httpOptions)
+    .pipe(
+      catchError(this.handleError<Pet[]>('favoritePets'))
+    );
+  }
+
+  addPetToBasket(name:string,pet:Pet):Observable<FavoritePets>{
+    const url = `${this.favoritePetsURL}/addPet/${name}`
+    return this.http.put<FavoritePets>(url,pet,this.httpOptions).pipe(
+      catchError(this.handleError<FavoritePets>('addpettoFavoritePets'))
+    );
+  }
+  
+  removePetFromBasket(name:string,petId:number):Observable<FavoritePets>{
+    const url = `${this.favoritePetsURL}/${name}/petId/${petId}`
+    return this.http.delete<FavoritePets>(url,this.httpOptions).pipe(
+      catchError(this.handleError<FavoritePets>('removePetFromBasket'))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
+  }
 }

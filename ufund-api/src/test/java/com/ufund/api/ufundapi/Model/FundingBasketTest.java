05658bbd39e46ad83d6742dd4594f1de82f21493
Rachel Adkins <rachelalaina1504@gmail.com>
2023-11-13 21:13:52 -0500

    Funding basket model tests

--- ufund-api/src/test/java/com/ufund/api/ufundapi/Model/FundingBasketTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/Model/FundingBasketTest.java
@@ -16,53 +16,52 @@ import com.ufund.api.ufundapi.model.Need;
 
 @Tag("Model-tier")
 public class FundingBasketTest {
-    // private FundingBasket fundingBasket;
+    private FundingBasket fundingBasket;
     
-    // @BeforeEach
-    // public void setUp(){
-    //     fundingBasket = new FundingBasket("carlitaxmessi",new HashMap<>());
-    // }
+    @BeforeEach
+    public void setUp(){
+        fundingBasket = new FundingBasket("carlitaxmessi",new HashMap<>());
+    }
 
-    // @Test
-    // public void testConstructor(){
-    //     assertEquals("carlitaxmessi",fundingBasket.getUserName());
-    //     assertEquals(new ArrayList<>(),fundingBasket.getFundingBasket());
+    @Test
+    public void testConstructor(){
+        assertEquals("carlitaxmessi",fundingBasket.getUserName());
+        assertEquals(new HashMap(),fundingBasket.getFundingBasket());
 
-    // }
+    }
 
+    @Test
+    public void getUsername(){
 
-    // @Test
-    // public void getUsername(){
+        assertEquals("carlitaxmessi",fundingBasket.getUserName());
+    }
 
-    //     assertEquals("carlitaxmessi",fundingBasket.getUserName());
-    // }
-
-    // @Test
-    // public void getFundingBasket(){
-    //     assertEquals(new HashMap<>(),fundingBasket.getFundingBasket());
-    // }
-
-
-    // // @Test
-    // // public void setFundingBasket(){
-    // //     Need need = new Need(1, "carla", 10, "hello", 10, "volunteer");
-    // //     fundingBasket.setFundingBasket(need);
-    // //     assertTrue(fundingBasket.getFundingBasket().contains(need));
-    // // }
+    @Test
+    public void getFundingBasket(){
+        assertEquals(new HashMap<>(),fundingBasket.getFundingBasket());
+    }
 
 
-    // @Test
-    // public void fundingBaskettoString(){
-    //     String stirng = "FundingBasket [userName=carlitaxmessi, fundingBasket=No items in the funding basket ]  ";
-    //     assertEquals(stirng,fundingBasket.toString());
-    // }
+    @Test
+    public void setFundingBasket(){
+        Need need = new Need(1, "carla", 10, 1, "hello", 10, "volunteer");
+        fundingBasket.setFundingBasket(need);
+        assertTrue(fundingBasket.getFundingBasket().containsValue(need));
+    }
 
-    // @Test 
-    // public void FundingBasketwithItems(){
-    //      Need need = new Need(1, "carla", 10, "hello", 10, "volunteer");
-    //     fundingBasket.setFundingBasket(need);
-    //     String string ="FundingBasket [userName=carlitaxmessi, fundingBasket=Need [id=1,name =carla, quantity = 10, description = hello, cost = 10, type = volunteer] ]  ";
-    //      assertEquals(string,fundingBasket.toString());
-    // }
+
+    @Test
+    public void fundingBaskettoString(){
+        String stirng = "FundingBasket [userName=carlitaxmessi, fundingBasket=No items in the funding basket ]  ";
+        assertEquals(stirng,fundingBasket.toString());
+    }
+
+    @Test 
+    public void FundingBasketwithItems(){
+        Need need = new Need(1, "carla", 10, 1, "hello", 10, "volunteer");
+        fundingBasket.setFundingBasket(need);
+        String string ="FundingBasket [userName=carlitaxmessi, fundingBasket=Need [id=1,name =carla, quantity = 10, surplus = 1, description = hello, cost = 10, type = volunteer] ]  ";
+        assertEquals(string,fundingBasket.toString());
+    }
 
 }
